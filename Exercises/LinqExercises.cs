fcfe2b4d2cf6891abdf83aa262d048b24d29b482
Krasimir Ronkov <s33592@pjwstk.edu.pl>
2026-03-26 23:43:18 +0100

    Task02_StudentEmailAddresses implemented

--- Exercises/LinqExercises.cs
+++ Exercises/LinqExercises.cs
@@ -30,7 +30,7 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task02_StudentEmailAddresses()
     {
-        throw NotImplemented(nameof(Task02_StudentEmailAddresses));
+       return UniversityData.Students.Select(s => s.Email).ToList();
     }
 
     /// <summary>
